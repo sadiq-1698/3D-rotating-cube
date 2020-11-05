979e0ced0f4fb1bef072dd28c3df0fe37062664f
sadiq-1698 <mohammedsadiq1698@gmail.com>
2020-11-05 20:01:58 +0530

    rotating

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const cube = document.getElementsByClassName('cube-container')[0];
+const totalcssRules = document.styleSheets[0].cssRules.length;
+const keyframes = document.styleSheets[0].cssRules[totalcssRules - 1];
+let animation1 = "cube-rotation 1s linear infinite";
+
+let X = 0;
+let Y = 0;
+let Z = 0;
+
+// keyframes.findRule('50%')
 
 cube.addEventListener('dragstart', dragStart);
 cube.addEventListener('dragend', dragEnd);
 
-function dragStart(){
+cube.addEventListener('onmouseover', onMouseOver);
+
+function dragStart(e) {
     console.log("started");
+    let {_x , _y} = onMouseOver(e);
+    X = _x % 360;
+    Y = _y % 360;
+    Z = (_x - _y) % 360;
+    cube.style.transform = `rotateX(${X}deg) rotateY(${Y}deg) rotateZ(${Z}deg)`;
 }
 
-function dragEnd(){
+function dragEnd() {
+    console.log(X + " " + Y + " " + Z);
     console.log("ended");
+}
+
+function onMouseOver(e) {
+    _x = e.clientX;
+    _y = e.clientY;
+    return {
+        _x,
+        _y
+    };
 }
 
 
